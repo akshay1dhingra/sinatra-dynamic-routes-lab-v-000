c207b1bccbac4120a3b1e95ebafd664e907d1bbd
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-07-30 19:49:39 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,40 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
   # Write your code here!
+
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name.reverse
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+    #@squared = (@number * @number) # or (@number**2) also will work
+    #@squared.to_s
+    (@number.to_i**2).to_s
+  end
+
+  get '/say/:number/:phrase' do # ask how this is working
+    string = ""
+    (params[:number].to_i).times do
+      string << "#{params[:phrase]}\n"
+      #binding.pry
+    end
+    string
+    #binding.pry
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+  end
+
+  get '/:operation/:number1/:number2' do 
+    operation = params[:operation]
+    number1 = params[:number1]
+    number2 = params[:number2]
+  end 
+
 
 end
